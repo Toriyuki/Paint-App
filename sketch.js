c1fc56862249bae7038374355558cff020df1316
Toriyuki <51471099+Toriyuki@users.noreply.github.com>
2022-01-30 02:39:00 -0600

    Lines and More
    Changes the dots into lines. Added the brush sizes and a reset button.

--- sketch.js
+++ sketch.js
@@ -1,41 +1,48 @@
 let selected_color;
 let num_of_colors;
-let color_box;
+let num_of_brushes;
 let colors;
-let over_color;
-
+let brushes_width;
+let over_color_box;
+let over_brush_box;
+let over_reset_box;
+let brush_width;  
 
 function setup() {
   createCanvas(1000, 600);
 
+  num_of_colors = 10;
+  num_of_brushes = 3;
+
+  fill(220);
+  noStroke();
   rect(0, 0, 1000, 600);
 
-  num_of_colors = 10;
-  color_box = rect(0, 0, 35, (num_of_colors * 30) + 5);
-
   colors = [color('red'), color('orange'), color('yellow'), color('lawngreen'), color('deepskyblue'),
-  color('blue'), color('violet'), color('brown'), color('white'), color('black')];
+  color('blue'), color('violet'), color('saddlebrown'), color('white'), color('black')];
   
+  brushes_width = [3, 5, 10];
+
   array_of_color_box = [];
-  setColorPalette();
   selected_color = color('black');
+
+  setUpToolArea();
+
+  brush_width = 5;
 }
 
-function draw() {
-  over_color = false;
-  if(mouseIsPressed) {
-    colorBoxTest();
-    if(over_color) {
-      inColorBox();
-    }
-    else {
-      fill(selected_color);
-      ellipse(mouseX, mouseY, 10, 10);
-    }
-  }
+function setUpToolArea() {
+  setColorBox();
+  setBrushBox();
+  setResetBox();
 }
 
-function setColorPalette() {
+function setColorBox() {
+  strokeWeight(1);
+  stroke(0);
+  fill(220);
+  rect(0, 0, 35, (num_of_colors * 30) + 5);
+
   noStroke();
   for (var i = 0; i < num_of_colors; i++) {
     fill(colors[i]);
@@ -44,15 +51,116 @@ function setColorPalette() {
 }
 
 function colorBoxTest() {
-  if((mouseX > 0) && (mouseX < 35) && (mouseY > 0) && (mouseY < 305)) {
-    over_color = true;
+  if ((mouseX > 0) && (mouseX < 35) && (mouseY > 0) && (mouseY < (num_of_colors * 30) + 5)) {
+    over_color_box = true;
+  }
+  else {
+    over_color_box = false;
   }
 }
 
 function inColorBox() {
   for(var i = 0; i < num_of_colors; i++) {
-    if((mouseX > 5) && (mouseX < 30) && (mouseY > (((i + 1) * 5) + (i * 25))) && (mouseY < (((i + 1) * 5) + ((i + 1) * 25)))) {
+    if ((mouseX > 5) && (mouseX < 30) && (mouseY > (((i + 1) * 5) + (i * 25))) && (mouseY < (((i + 1) * 5) + ((i + 1) * 25)))) {
       selected_color = colors[i];
     }
   }
+}
+
+function setBrushBox() {
+  strokeWeight(1);
+  stroke(0);
+  fill(220);
+  num_of_colors = 10;
+  for (var i = 0; i < num_of_brushes; i++) {
+    rect(35, i * 30, 60, 30);
+  }
+
+  for (var i = 0; i < num_of_brushes; i++) {
+    strokeWeight(brushes_width[i]);
+    line(45, i * 30 + 15, 85, i * 30 + 15);
+  }
+}
+
+function brushBoxTest() {
+  if ((mouseX > 35) && (mouseX < 95) && (mouseY > 0) && (mouseY < num_of_brushes * 30)) {
+    over_brush_box = true;
+  }
+  else {
+    over_brush_box = false;
+  }
+}
+
+function inBrushBox() {
+  for (var i = 0; i < num_of_brushes; i++) {
+    if ((mouseX > 35) && (mouseX < 95) && (mouseY > (i * 30)) && (mouseY < ((i + 1) * 30))) {
+      brush_width = brushes_width[i];
+    }
+  }
+}
+
+function setResetBox() {
+  strokeWeight(1);
+  stroke(0);
+  fill(220);
+  rect(95, 0, 60, 30);
+
+  textSize(18);
+  fill(0);
+  text('Reset', 100, 20);
+}
+
+function resetBoxTest() {
+  if ((mouseX > 95) && (mouseX < 155) && (mouseY > 0) && (mouseY < 30)) {
+    over_reset_box = true;
+  }
+  else {
+    over_reset_box = false;
+  }
+}
+
+function inResetBox() {
+  fill(220);
+  noStroke();
+  rect(0, 0, 1000, 600);
+
+  setUpToolArea();
+}
+
+function runAllTest() {
+  colorBoxTest();
+  brushBoxTest();
+  resetBoxTest();
+}
+
+function mousePressed() {
+  runAllTest();
+  if (over_color_box) {
+    inColorBox();
+  }
+  else if (over_brush_box) {
+    inBrushBox();
+  }
+  else if (over_reset_box) {
+    inResetBox();
+  }
+  else {
+    strokeWeight(brush_width);
+    stroke(selected_color);
+    strokeJoin(ROUND);
+    beginShape(LINES);
+    vertex(mouseX, mouseY);
+  }
+}
+
+function mouseDragged() {
+  vertex(mouseX, mouseY);
+  endShape();
+  beginShape(LINES);
+  vertex(mouseX, mouseY);
+}
+
+function mouseReleased() {
+  endShape();
+  setUpToolArea();
 }
