7d7f49a59bd3f6beba1df528381cf7fb6500af7c
Toriyuki <51471099+Toriyuki@users.noreply.github.com>
2022-01-30 14:52:13 -0600

    Made a settings button and added comments

--- sketch.js
+++ sketch.js
@@ -8,11 +8,18 @@ let over_brush_box;
 let over_reset_box;
 let brush_width;  
 
+//Sets the default settings of the paint app
+function settings() {
+  num_of_colors = 10;
+  num_of_brushes = 3;
+  brush_width = 5;
+}
+
+//Sets up the canvas and loads the tool bar
 function setup() {
   createCanvas(1000, 600);
 
-  num_of_colors = 10;
-  num_of_brushes = 3;
+  settings();
 
   fill(220);
   noStroke();
@@ -27,16 +34,16 @@ function setup() {
   selected_color = color('black');
 
   setUpToolArea();
-
-  brush_width = 5;
 }
 
+//Sets the tool area on the canvas
 function setUpToolArea() {
   setColorBox();
   setBrushBox();
   setResetBox();
 }
 
+//Sets the color selector on the canvas
 function setColorBox() {
   strokeWeight(1);
   stroke(0);
@@ -50,6 +57,7 @@ function setColorBox() {
   }
 }
 
+//Test if the mouse is over the color box
 function colorBoxTest() {
   if ((mouseX > 0) && (mouseX < 35) && (mouseY > 0) && (mouseY < (num_of_colors * 30) + 5)) {
     over_color_box = true;
@@ -59,6 +67,7 @@ function colorBoxTest() {
   }
 }
 
+//Selects the color that the mouse is over
 function inColorBox() {
   for(var i = 0; i < num_of_colors; i++) {
     if ((mouseX > 5) && (mouseX < 30) && (mouseY > (((i + 1) * 5) + (i * 25))) && (mouseY < (((i + 1) * 5) + ((i + 1) * 25)))) {
@@ -67,6 +76,7 @@ function inColorBox() {
   }
 }
 
+//Sets the brush options in on the canvas
 function setBrushBox() {
   strokeWeight(1);
   stroke(0);
@@ -82,6 +92,7 @@ function setBrushBox() {
   }
 }
 
+//Test if mouse is over the brush boxes
 function brushBoxTest() {
   if ((mouseX > 35) && (mouseX < 95) && (mouseY > 0) && (mouseY < num_of_brushes * 30)) {
     over_brush_box = true;
@@ -91,6 +102,7 @@ function brushBoxTest() {
   }
 }
 
+//Selects the brush size based on where the mouse is over
 function inBrushBox() {
   for (var i = 0; i < num_of_brushes; i++) {
     if ((mouseX > 35) && (mouseX < 95) && (mouseY > (i * 30)) && (mouseY < ((i + 1) * 30))) {
@@ -99,6 +111,7 @@ function inBrushBox() {
   }
 }
 
+//Sets the reset button on the canvas
 function setResetBox() {
   strokeWeight(1);
   stroke(0);
@@ -110,6 +123,7 @@ function setResetBox() {
   text('Reset', 100, 20);
 }
 
+//Test to see if the mouse is over the reset button
 function resetBoxTest() {
   if ((mouseX > 95) && (mouseX < 155) && (mouseY > 0) && (mouseY < 30)) {
     over_reset_box = true;
@@ -119,6 +133,7 @@ function resetBoxTest() {
   }
 }
 
+//Resets the canvas
 function inResetBox() {
   fill(220);
   noStroke();
@@ -127,12 +142,16 @@ function inResetBox() {
   setUpToolArea();
 }
 
+//Calls all test to see where the mouse is
 function runAllTest() {
   colorBoxTest();
   brushBoxTest();
   resetBoxTest();
 }
 
+//When mouse button is pressed, detects where the mouse is.
+//If mouse is in tool bar, do what the mouse is over.
+//If mouse is not in tool bar, starts drawing.
 function mousePressed() {
   runAllTest();
   if (over_color_box) {
@@ -153,10 +172,11 @@ function mousePressed() {
   }
 }
 
+//Continue drawing unless mouse goes over the tool bar.
 function mouseDragged() {
   runAllTest();
   if(over_color_box || over_brush_box || over_reset_box) {
-
+    endShape();
   }
   else {
     vertex(mouseX, mouseY);
@@ -166,6 +186,7 @@ function mouseDragged() {
   }
 }
 
+//Stops drawing when makes sure drawings are not on top of tool bar
 function mouseReleased() {
   endShape();
   setUpToolArea();
